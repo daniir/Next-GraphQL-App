fc2ca271c3b5525f5a869d331e4450ca331c7e97
daniir <danrodas03@gmail.com>
2022-10-07 16:02:54 -0500

    Implementación de resolvers

--- src/graphql/resolvers/taskResolvers.ts
+++ src/graphql/resolvers/taskResolvers.ts
@@ -1,14 +1,42 @@
 import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import { TaskService } from '../../services/taskService';
 import { Task } from '../entities/taskEntity';
 import { TaskInput } from '../inputs/taskInputs';
 
 @Resolver()
 export class TaskResolver
 {
-    constructor(){};
+    constructor(
+        private readonly taskService = new TaskService()
+    ){};
 
-    // @Mutation(() => String)
-    // createTask(projectId: string, payload: TaskInput){
+    @Query(() => [Task])
+    getTasks(){
+        return this.taskService.getTask();
+    }
 
-    // }
+    @Mutation(() => Task)
+    createTask(
+        @Arg('payload') payload: TaskInput
+    ): Task{
+        const task = this.taskService.addTask(payload);
+        return task;
+    };
+
+    @Mutation(() => String)
+    updateTask(
+        @Arg('id') id: string,
+        @Arg('name') name: string,
+    ): String{
+        const response = this.taskService.updateTask(id, name);
+        return response;
+    };
+
+    @Mutation(() => String)
+    deleteTask(
+        @Arg('id') id: string,
+    ): String{
+        const response = this.taskService.deleteTask(id);
+        return response;
+    }
 };
