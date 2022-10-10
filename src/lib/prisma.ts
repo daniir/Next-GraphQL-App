e3277e8fbbdeff262c8d77b1a01faa228a84c5be
daniir <danrodas03@gmail.com>
2022-10-10 14:34:04 -0500

    Corrección de error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV === 'production'){
     prisma = new PrismaClient();
 } else {
     if(!global.prisma){
-        global.print = new PrismaClient();
+        global.prisma = new PrismaClient();
     };
 
     prisma = global.prisma;
