cb0ba06d174d6b4bf9509fbc9b5594a5a0955b72
daniir <danrodas03@gmail.com>
2022-10-10 15:56:49 -0500

    Llamado a base de datos

--- src/graphql/resolvers/projectResolvers.ts
+++ src/graphql/resolvers/projectResolvers.ts
@@ -1,52 +1,55 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import { Resolver, Query, Mutation, Arg, Ctx } from 'type-graphql';
 import { ProjectService } from '../../services/projectService';
-import { Project, ProjectWithTasks } from '../entities/projectEntity';
+import type { Context } from '../context';
+import { Project } from '../entities/projectEntity';
 import { ProjectInput } from '../inputs/projectInputs';
 
 @Resolver()
 export class ProjectResolver
 {
-
-    constructor( 
-        private readonly projectService = new ProjectService()
-    ){}
-
     @Query(() => [Project])
-    getProjects(){
-        const projects = this.projectService.getProjects();
-        return projects;
+    async getProjects(
+        @Ctx() ctx: Context,
+    ) {
+        try {
+            const projects = await ctx.prisma.projects.findMany();
+            if (!projects) return [];
+            return projects;
+        } catch (error) {
+            console.error(`Query getAll Error: ${error}`);
+        }
     };
 
     @Query(() => Project)
-    getProject(
-        @Arg('id') id: string
-    ){
-        const project = this.projectService.getProject(id);
-        return project;
-    };
-
-    @Mutation(() => Project)
-    createProject(
-        @Arg('payload') payload: ProjectInput
-    ){
-        const project = this.projectService.createProject(payload);
-        return project;
-    };
-
-    @Mutation(() => Project)
-    updateProject(
+    async getProject(
         @Arg('id') id: string,
-        @Arg('payload') payload: ProjectInput
+        @Ctx() ctx: Context
     ){
-        const project = this.projectService.updateProject(id, payload);
-        return project;
+        try {
+            const project = await ctx.prisma.projects.findFirstOrThrow({ where: { id } });
+            if(!project) throw new Error(`Project with id: ${id} not found`);
+            return project;
+        } catch (error) {
+            console.error(`Query getOne Error: ${error}`);
+        }
     };
-
-    @Mutation(() => String)
-    deleteProject(
-        @Arg('id') id: string
+    
+    @Mutation(() => Project)
+    async createProject(
+        @Arg('payload') payload: ProjectInput,
+        @Ctx() ctx: Context,
     ){
-        const project = this.projectService.deleteProject(id);
-        return project;
-    };
+        try {
+            const { name, description } = payload;
+            const project = await ctx.prisma.projects.create({
+                data: {
+                    name,
+                    description,
+                },
+            });
+            return project;
+        } catch (error) {
+            console.error(`Mutation create Error: ${error}`);
+        }
+    }
 };
