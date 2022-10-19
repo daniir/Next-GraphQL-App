ef16e6073aac7385110483295000bc5ab7c7ccae
daniir <danrodas03@gmail.com>
2022-10-18 21:42:38 -0500

    ApolloProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout/Layout';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../src/lib/apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Layout>
+    <ApolloProvider client={ client }>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  </Layout>
 }
 
 export default MyApp
