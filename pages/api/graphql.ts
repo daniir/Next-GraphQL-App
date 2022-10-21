209c4535eaad60349ac8ddf1101ac78bc1bf8d57
daniir <danrodas03@gmail.com>
2022-10-21 15:57:00 -0500

    GraphQL Server

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-micro';
 import { buildSchema } from 'type-graphql';
-import type { PageConfig } from 'next'
+import type { PageConfig } from 'next';
 import Cors from 'micro-cors';
 import createContext from '../../src/graphql/context';
 import { ProjectResolver } from '../../src/graphql/resolvers/projectResolvers';
@@ -9,28 +9,28 @@ import { TaskResolver } from '../../src/graphql/resolvers/taskResolvers';
 
 const cors = Cors();
 
-export const config: PageConfig = {
-    api:{
-        bodyParser: false,
-    },
-};
-
 const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-        resolvers: [ProjectResolver, TaskResolver]
-    }),
-    context: createContext,
-    csrfPrevention: true,
+  schema: await buildSchema({
+    resolvers: [ProjectResolver, TaskResolver],
+  }),
+  context: createContext,
+  csrfPrevention: true,
 });
 
 const startServer = apolloServer.start();
 
-export default cors(async function handler(req, res){
-    if(req.method === 'OPTIONS'){
-        res.end();
-        return false;
-    };
+export default cors(async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
 
-    await startServer;
-    await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+  await startServer;
+  await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
 });
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
