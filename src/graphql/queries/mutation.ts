df9f738ddb87d1aae15c545492121d7ec544b808
daniir <danrodas03@gmail.com>
2022-10-22 22:02:29 -0500

    GQL Mutations

--- src/graphql/queries/mutation.ts
+++ src/graphql/queries/mutation.ts
@@ -1,11 +1,25 @@
 import { gql } from '@apollo/client';
 
+//Project Mutations
+
 export const CREATE_PROJECT = gql`
-    mutation CreateProject($payload: ProjectInput!){
+    mutation ($payload: ProjectInput!){
         createProject(payload: $payload){
             id
             name
             description
         }
+    }
+`;
+
+export const UPDATE_PROJECT = gql`
+    mutation ($id: String!, $payload: ProjectInput!){
+        updateProject(id: $id, payload: $payload)
+    }
+`;
+
+export const DELETE_PROJECT = gql`
+    mutation ($id: String!){
+        deleteProject(id: $id)
     }
 `;
