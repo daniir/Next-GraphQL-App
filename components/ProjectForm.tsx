b52bd0786cf4b12ce82032307d9f0a138e66a6cb
daniir <danrodas03@gmail.com>
2022-10-22 21:58:22 -0500

    Form component

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -9,48 +9,58 @@ export default function ProjectForm(){
 
   const {
     form,
+    errorMsg,
+    loading,
     handlerChange,
     handlerSubmit
   } = useForm(projectForm);
 
 
-    return (
+    return loading ? (
+      <div>
+        <p>...loading...</p>
+      </div>
+    ) : (
       <form onSubmit={handlerSubmit}>
-        <div className='row justify-content-center'>
-          <div className='mb-3'>
-            <label htmlFor='formName' className='form-label'>
+        <div className="row justify-content-center">
+          <div className="mb-3">
+            <label htmlFor="formName" className="form-label">
               Name
             </label>
             <input
-              className='form-control w-25'
-              type='text'
-              name='name'
-              id='formName'
+              className="form-control w-25"
+              type="text"
+              name="name"
+              placeholder="Add a project name"
+              id="formName"
               value={form.name}
               onChange={handlerChange}
             />
+            {
+              errorMsg && <p className="text-danger">{errorMsg}</p>
+            }
           </div>
-          <div className='col align-self-center'>
-            <div className='mb-3'>
-              <label htmlFor='formDescription' className='form-label'>
+          <div className="col align-self-center">
+            <div className="mb-3">
+              <label htmlFor="formDescription" className="form-label">
                 Description
               </label>
-              <input
-                className='form-control w-50'
-                type='text'
-                name='description'
-                id='formDescription'
+              <textarea
+                className="form-control w-50"
+                name="description"
+                id="formDescription"
+                rows={3}
                 value={form.description}
                 onChange={handlerChange}
-              />
+              ></textarea>
             </div>
           </div>
           <div>
-            <button type='submit' className='btn btn-primary'>
+            <button type="submit" className="btn btn-primary">
               Create
             </button>
           </div>
         </div>
       </form>
-    );
+    ); 
 };
