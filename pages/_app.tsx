3b541562d3d3e9ff5927498ffff564edda96d19e
daniir <danrodas03@gmail.com>
2022-10-23 22:10:23 -0500

    Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,26 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout/Layout';
 import { ApolloProvider } from '@apollo/client';
 import { client } from '../src/lib/apollo';
+import { ProjectProvider } from '../context/ProjectProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return (
     <ApolloProvider client={client}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ProjectProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ProjectProvider>
     </ApolloProvider>
   );
 };
