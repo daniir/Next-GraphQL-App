986db8a812627545fc02bd4d3d594105f31fd306
daniir <danrodas03@gmail.com>
2022-10-23 22:12:03 -0500

    Index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,46 @@
-import type { NextPage } from 'next';
+import GoTo from '../components/GoTo';
+import Project from '../components/Project';
+import { GET_PORJECTS } from '../src/graphql/data/query';
+import { ServerSideIndex } from '../src/graphql/data/types';
+import { client } from '../src/lib/apollo';
 
-const index: NextPage = () => {
+export default function Index({ projects, loading }: ServerSideIndex) {
+
+  if(loading) return <p>..loading...</p>
+
   return(
-    <div className='container'>
-      <h1>Home</h1>
+    <div className="text-center mt-2">
+      <h3>Project List</h3>
+      <GoTo path='project/create' name='Add new project'/>
+      <hr />
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {
+          projects.length > 0 ? (
+            projects.map(
+              project => (
+                <div className="col" key={project.id}>
+                <Project project={project}/>
+              </div>
+              )
+            )
+          ) : (
+            <p>No projects</p>
+          )
+        }
+      </div>
     </div>
   )
-}
+};
 
-export default index;
+export async function getServerSideProps() {
+  const { data, loading } = await client.query({
+    query: GET_PORJECTS
+  });
+
+  return {
+    props: {
+      projects: data.getProjects,
+      loading
+    }
+  }
+};
