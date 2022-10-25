4dd92854c86f95418f4a8e9730c591f8d7400109
daniir <danrodas03@gmail.com>
2022-10-24 21:57:35 -0500

    Components

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,49 +2,47 @@ import Link from "next/link";
 import { ProjectProp } from "../src/graphql/data/types";
 import { useMutation } from '@apollo/client';
 import { DELETE_PROJECT } from "../src/graphql/data/mutation";
-import { useContext } from "react";
-import ProjectContext from "../context/ProjectProvider";
 
-export default function Card({ project }: ProjectProp){
+export default function Card({ project }: ProjectProp) {
+  const [deleteProject] = useMutation(DELETE_PROJECT);
 
-    const [deleteProject] =  useMutation(DELETE_PROJECT);
-    const { isEdit, setIsEdit } = useContext(ProjectContext);
+  const removeProject = async (id: string) => {
+    await deleteProject({
+      variables: {
+        id,
+      },
+    });
+  };
 
-    const removeProject = (id: string) => {
-      deleteProject({
-        variables: {
-          id
-        },
-      });
-    };
-
-    const editProject = (project: ProjectProp) => {
-      setIsEdit(project);
-    };
-
-    return(
-        <div className="card" style={{ width: "18rem" }}>
-        <div className="card-body">
-          <h5 className="card-title">{project.name}</h5>
-          <p className="card-text">{project.description}</p>
-          <Link href={`/project/${project.id}`}>
-            <a style={{ textDecoration: "none" }}>
-              Manage project
-              <i className="mx-2 bi bi-arrow-right-circle"></i>
-            </a>
-          </Link>
-        </div>
-        <div className="card-footer">
-          <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button className="btn btn-warning btn-sm">
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h5 className="card-title">{project.name}</h5>
+        <p className="card-text">{project.description}</p>
+        <Link href={`/project/${project.id}`}>
+          <a style={{ textDecoration: "none" }}>
+            Manage project
+            <i className="mx-2 bi bi-arrow-right-circle"></i>
+          </a>
+        </Link>
+      </div>
+      <div className="card-footer">
+        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+          <Link href={`/project/edit/${project.id}`}>
+            <button
+              className="btn btn-warning btn-sm"
+            >
               <i className="bi bi-pencil-fill"></i>
             </button>
-            <button className="btn btn-danger btn-sm"
-              onClick={() => removeProject(project.id)}>
-              <i className="bi bi-trash2-fill"></i>
-            </button>
-          </div>
+          </Link>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => removeProject(project.id)}
+          >
+            <i className="bi bi-trash2-fill"></i>
+          </button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,4 +1,11 @@
+import React, { useState, useEffect } from 'react';
 import { useForm } from '../hooks/useForm';
+
+type initialData = {
+  id?: string,
+  name: string,
+  description?: string,
+};
 
 const projectForm = {
   name: '',
@@ -7,60 +14,54 @@ const projectForm = {
 
 export default function ProjectForm(){
 
-  const {
-    form,
-    errorMsg,
-    loading,
-    handlerChange,
-    handlerSubmit
-  } = useForm(projectForm);
+  const [data, setData] = useState<initialData>(projectForm);
 
+  const { form, errorMsg, loading, handlerChange, handlerSubmit } =
+    useForm(data);
 
-    return loading ? (
-      <div>
-        <p>...loading...</p>
-      </div>
-    ) : (
-      <form onSubmit={handlerSubmit}>
-        <div className="row justify-content-center">
+  return loading ? (
+    <div>
+      <p>...loading...</p>
+    </div>
+  ) : (
+    <form onSubmit={handlerSubmit}>
+      <div className="row justify-content-center">
+        <div className="mb-3">
+          <label htmlFor="formName" className="form-label">
+            Name
+          </label>
+          <input
+            className="form-control w-25"
+            type="text"
+            name="name"
+            placeholder="Add a project name"
+            id="formName"
+            value={form.name}
+            onChange={handlerChange}
+          />
+          {errorMsg && <p className="text-danger">{errorMsg}</p>}
+        </div>
+        <div className="col align-self-center">
           <div className="mb-3">
-            <label htmlFor="formName" className="form-label">
-              Name
+            <label htmlFor="formDescription" className="form-label">
+              Description
             </label>
-            <input
-              className="form-control w-25"
-              type="text"
-              name="name"
-              placeholder="Add a project name"
-              id="formName"
-              value={form.name}
+            <textarea
+              className="form-control w-50"
+              name="description"
+              id="formDescription"
+              rows={3}
+              value={form.description}
               onChange={handlerChange}
-            />
-            {
-              errorMsg && <p className="text-danger">{errorMsg}</p>
-            }
-          </div>
-          <div className="col align-self-center">
-            <div className="mb-3">
-              <label htmlFor="formDescription" className="form-label">
-                Description
-              </label>
-              <textarea
-                className="form-control w-50"
-                name="description"
-                id="formDescription"
-                rows={3}
-                value={form.description}
-                onChange={handlerChange}
-              ></textarea>
-            </div>
-          </div>
-          <div>
-            <button type="submit" className="btn btn-primary">
-              Create
-            </button>
+            ></textarea>
           </div>
         </div>
-      </form>
-    ); 
+        <div>
+          <button type="submit" className="btn btn-primary">
+            Create
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 };
