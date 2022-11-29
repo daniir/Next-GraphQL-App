d0485d4875ebccf8b7ebf7222ed0db5c9b4c8f15
Daniel Rodas <danrodas03@gmail.com>
2022-11-28 22:08:57 -0500

    Project Page

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -1,35 +1,27 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import { GET_PROJECT_WITH_TASKS } from '../../src/graphql/data/query';
-import { PorjectWithTasks } from '../../src/graphql/data/types';
+import { ServerSideProject } from '../../src/graphql/data/types';
 import Details from '../../components/Detail';
 import { client } from '../../src/lib/apollo';
 
-export default function Edit(){
-
-    const router = useRouter();
-    const { id } = router.query;
-    const [project, setProject] = useState<PorjectWithTasks>();
-    const [loading, setLoading] = useState(false);
-    
-    useEffect(() => {
-        const res = async()=> {
-            const { data, loading } = await client.query({
-                query: GET_PROJECT_WITH_TASKS,
-                variables: { id },
-            });
-            setProject(data.getProjectWithTask);
-            setLoading(loading);
-        };
-
-        res();
-    }, [id]);
+export default function Edit({ project, loading }: ServerSideProject){
 
     if (loading) return <p>...loading...</p>
 
     return(
-        project 
-        ? <Details project={project}/>
-        : null
+        <Details project={project}/>
     )
+};
+
+export async function getServerSideProps({ query: { id } }) {
+    const { data, loading } = await client.query({
+        query: GET_PROJECT_WITH_TASKS,
+        variables: { id }
+    });
+
+    return {
+        props: {
+            project: data.getProjectWithTask,
+            loading
+        }
+    }
 };
--- pages/project/edit/[id].tsx
+++ pages/project/edit/[id].tsx
@@ -1,4 +1,4 @@
-import { GET_PROJECT } from '../../../src/graphql/data/query';
+import { GET_PROJECT_WITH_TASKS } from '../../../src/graphql/data/query';
 import { ServerSideIndex } from '../../../src/graphql/data/types';
 import { client } from '../../../src/lib/apollo';
 import ProjectForm from '../../../components/ProjectForm';
@@ -15,13 +15,13 @@ export default function Edit({ project, loading }: ServerSideIndex){
 
 export async function getServerSideProps({ query: { id }}){
     const { data, loading } = await client.query({
-        query: GET_PROJECT,
+        query: GET_PROJECT_WITH_TASKS,
         variables: { id }
     });
 
     return{
         props: {
-            project: data.getProject,
+            project: data.getProjectWithTask,
             loading
         }
     }
